431ce91fe0e1731ba5042cd0dafda239506588b4
MalconMouraLima <malcon.moura.lima@gmail.com>
2017-06-20 13:06:28 -0300

    add form

--- app/models/jewel.rb
+++ app/models/jewel.rb
@@ -3,7 +3,10 @@ class Jewel < ApplicationRecord
   belongs_to :category
   has_many :reviews
 
-  has_attached_file :jewel_img, styles: { jewel_index: "250x250>", jewel_show: "325x475>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :jewel_img,
+                    styles: { jewel_index: "250x250>",
+                              jewel_show: "325x475>" }
+
   validates_attachment_content_type :jewel_img, content_type: /\Aimage\/.*\z/
 
 end
